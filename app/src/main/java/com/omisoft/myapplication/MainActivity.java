3b0ca19f4ada3640a96c9d02deec18b9080c671e
y.futysh <yuriy.futysh@fluvius.co>
2021-11-08 09:41:07 +0200

    Added read asset txt

--- app/src/main/java/com/omisoft/myapplication/MainActivity.java
+++ app/src/main/java/com/omisoft/myapplication/MainActivity.java
@@ -2,14 +2,26 @@ package com.omisoft.myapplication;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
+
+  TextView title;
 
   @SuppressLint("SetJavaScriptEnabled")
   @Override
@@ -17,12 +29,13 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    title = findViewById(R.id.txt_title);
     final AppCompatButton secondActivityButton = findViewById(R.id.btn_second_activity);
     final AppCompatButton openGoogleButton = findViewById(R.id.btn_open_browser);
     final WebView webView = findViewById(R.id.web_view);
 
     secondActivityButton.setOnClickListener(view -> {
-      final Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+      final Intent intent = new Intent(MainActivity.this, SecondActivity.class);
       startActivity(intent);
     });
 
@@ -30,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
       webView.getSettings().setJavaScriptEnabled(true);
       webView.getSettings().setLoadWithOverviewMode(true);
       webView.getSettings().setUseWideViewPort(true);
-      webView.setWebViewClient(new WebViewClient(){
+      webView.setWebViewClient(new WebViewClient() {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -38,11 +51,34 @@ public class MainActivity extends AppCompatActivity {
 
           return true;
         }
+
         @Override
         public void onPageFinished(WebView view, final String url) {
         }
       });
       webView.loadUrl("https://www.google.com/");
     });
+  }
+
+  void readTextAsset() {
+    try {
+      AssetManager am = this.getAssets();
+      InputStream is = am.open("test.txt");
+
+      StringBuilder textBuilder = new StringBuilder();
+      try (Reader reader = new BufferedReader(new InputStreamReader
+              (is, Charset.forName(StandardCharsets.UTF_8.name())))) {
+        int c;
+        while ((c = reader.read()) != -1) {
+          textBuilder.append((char) c);
+        }
+      }
+
+      title.setText(textBuilder.toString());
+
+      is.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
